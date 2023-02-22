70d86328f7cb3d6d42e6b458261dc542fd26f088
herbeysoft-team <herbeysoft@gmail.com>
2023-02-22 13:12:14 +0100

    Authentication Completed

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -4,9 +4,15 @@ const jwt = require("jsonwebtoken");
 const { TOKEN_KEY, TOKEN_EXPIRY } = process.env;
 var bcrypt = require("bcryptjs");
 const db = require("../config/database");
-const generateSMS = require('../utilities/generateSMS')
-const { sendOTP, verifyOTP, sendVerificationOTPPhone } = require('../utilities/sendOTP')
-
+const generateSMS = require("../utilities/generateSMS");
+const {
+  sendOTP,
+  verifyOTP,
+  sendVerificationOTPPhone,
+  verifyUserPhone,
+  sendPasswordResetOTPPhone,
+  resetUserPassword,
+} = require("../utilities/sendOTP");
 
 
 /**
@@ -16,7 +22,7 @@ const { sendOTP, verifyOTP, sendVerificationOTPPhone } = require('../utilities/s
  * gender - Get the gender, male or female
  * phone_no - Get the phone number (unique)
  * password - Get the password
- * 
+ *
  */
 exports.signup = async (req, res) => {
   let { fullname, city, gender, password, phone_no, username } = req.body;
@@ -41,11 +47,11 @@ exports.signup = async (req, res) => {
       "INSERT INTO userProfile (fullname, username, phone_no, city, password, gender) VALUES (?,?,?,?,?,?)",
       [fullname, username, phone_no, city, hashedPassword, gender]
     );
-    if(result){
-    const otp = await sendVerificationOTPPhone(phone_no);
-    res
-      .status(201)
-      .json({ result, info: "User has been created Successfully", otp });
+    if (result) {
+      const otp = await sendVerificationOTPPhone(phone_no);
+      res
+        .status(201)
+        .json({ result, info: "User has been created Successfully", otp });
     }
   } catch (error) {
     res.status(500).json({ message: "something went wrong" });
@@ -58,44 +64,49 @@ exports.signup = async (req, res) => {
  * POST - http://localhost:8000/api/v1/auth/login
  * phone_no - Get the phone number (unique)
  * password - Get the password
- * 
+ *
  */
 exports.login = async (req, res) => {
   try {
-  
     //CHECK IF THE INPUT IS NOT EMPTY
-    if(!(req.body.phone_no && req.body.password)){
+    if (!(req.body.phone_no && req.body.password)) {
       throw Error("Empty Credentials Supplied");
     }
 
     //CHECK IF THE USER EXIST
     const userExist = await db.getrow(
       "SELECT * FROM userprofile  WHERE phone_no = ? OR username = ? ",
-      [ req.body.phone_no, req.body.phone_no ]
+      [req.body.phone_no, req.body.phone_no]
     );
 
-    
     //IF USER DOES NOT EXIST
-    if(!userExist){
+    if (!userExist) {
       return res.status(404).json({ message: "user does not exist" });
     }
 
-    if(!userExist.verified){
-      return res.status(404).json({ message: "Phone Number hasn't be verified yet. Please check your inbox" });
+    if (!userExist.verified) {
+      return res
+        .status(404)
+        .json({
+          message:
+            "Phone Number hasn't be verified yet. Please check your inbox",
+        });
     }
     //IF USER EXIST
-    const hashedPassword  = userExist.password;
-    
-    //CHECK IF PASSWORD MATCH
-    const passwordMatch = await verifyHashData(req.body.password, hashedPassword)
-    
+    const hashedPassword = userExist.password;
 
-    if(!passwordMatch){
+    //CHECK IF PASSWORD MATCH
+    const passwordMatch = await verifyHashData(
+      req.body.password,
+      hashedPassword
+    );
+
+    if (!passwordMatch) {
       return res.status(404).json({ message: "Password in incorrect" });
     }
 
     //CREATE USER TOKEN
-    const tokenData = {userId: userExist.id, phone_no: userExist.phone_no };
+    const tokenData = { userId: userExist.id, phone_no: userExist.phone_no };
     const token = await createToken(tokenData);
 
     //DESTRUCTION USER EXIST INFO
@@ -107,62 +118,140 @@ exports.login = async (req, res) => {
       })
       .status(200)
       .json(others);
-
   } catch (error) {
     res.status(500).json({ message: "something went wrong" });
     console.log(error);
-    
   }
-}
+};
 
 
 /**
  * POST - http://localhost:8000/api/v1/auth/logout
  */
 exports.logout = async (req, res) => {
-  res.clearCookie("access_token",{
-    secure:true,
-    sameSite:"none"
-  }).status(200).json("User has been logged out.")
-}
+  res
+    .clearCookie("access_token", {
+      secure: true,
+      sameSite: "none",
+    })
+    .status(200)
+    .json("User has been logged out.");
+};
 
 
 /**
  * POST - http://localhost:8000/api/v1/auth/verify
  * phone_no - Get the phone number (unique)
  * otp - Get the otp
- * 
+ *
  */
-exports.verify =async(req, res) => {
+exports.verify = async (req, res) => {
   try {
-      let  { phone_no, otp } = req.body;
+    let { phone_no, otp } = req.body;
 
-     const validOTP = await verifyOTP({phone_no, otp});
-     res.status(200).json({valid: validOTP});
+    const validOTP = await verifyOTP({ phone_no, otp });
+    res.status(200).json({ valid: validOTP });
   } catch (error) {
-      res.status(400).send(error.message);
+    res.status(400).send(error.message);
   }
-}
+};
+
 
 /**
  * POST - http://localhost:8000/api/v1/auth/otp
  * phone_no - Get the phone number (unique)
- * message - the message to be sent 
+ * message - the message to be sent
  * duration: the time the otp will take to expire
- * 
+ *
  */
-exports.otp = async(req, res) => {
+exports.otp = async (req, res) => {
   try {
-      const { phone_no, message, duration } = req.body;
+    const { phone_no, message, duration } = req.body;
 
-      const createdOTP = await sendOTP({
-          phone_no,
-          message,
-          duration
-      });
-      console.log(createdOTP)
-      res.status(200).json(createdOTP);
+    const createdOTP = await sendOTP({
+      phone_no,
+      message,
+      duration,
+    });
+    console.log(createdOTP);
+    res.status(200).json(createdOTP);
   } catch (error) {
-      res.status(400).send(error.message);
+    res.status(400).send(error.message);
   }
-}
+};
+
+
+/**
+ * POST - http://localhost:8000/api/v1/auth/passwordreset
+ * phone_no - Get the phone number (unique)
+ *
+ */
+exports.passwordreset = async (req, res) => {
+  try {
+    const { phone_no } = req.body;
+    if (!phone_no) throw Error("Phone number is required");
+
+    const createPasswordResetOTP = await sendPasswordResetOTPPhone(phone_no);
+    res.status(200).json(createPasswordResetOTP);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
+
+/**
+ * POST - http://localhost:8000/api/v1/auth/reset
+ * phone_no - Get the phone number (unique)
+ * otp - the new otp recieved
+ * newPassword - the new passowrd
+ */
+exports.reset = async (req, res) => {
+  try {
+    const { phone_no, otp, newPassword } = req.body;
+    if (!(phone_no && otp && newPassword))
+      throw Error("Phone number is required");
+
+    await resetUserPassword({ phone_no, otp, newPassword });
+    res.status(200).json({ phone_no, passwordreset: true });
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
+
+/**
+ * POST - http://localhost:8000/api/v1/auth/verifyphoneotp
+ * phone_no - Get the phone number (unique)
+ * otp - Get the otp
+ *
+ */
+exports.verifyphoneotp = async (req, res) => {
+  try {
+    const { phone_no, otp } = req.body;
+    if (!phone_no && otp) throw Error("Empty otp details are not allowed");
+
+    const verifiedPhone = await verifyUserPhone({ phone_no, otp });
+    res.status(200).json({ phone_no, verified: true, verifiedPhone });
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
+
+/**
+ * POST - http://localhost:8000/api/v1/auth/phoneotp
+ * phone_no - Get the phone number (unique)
+ *
+ */
+exports.phoneotp = async (req, res) => {
+  try {
+    const { phone_no } = req.body;
+    if (!phone_no) throw Error("A Phone number is required");
+
+    const createPhoneVerificationOTP = await sendVerificationOTPPhone(phone_no);
+    console.log(createPhoneVerificationOTP);
+    res.status(200).json(createPhoneVerificationOTP);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,13 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const verifyToken = require("../middleware/verifyToken");
-const { signup, login, logout, verify, otp } = require("../controller/auth");
-const {
-  sendVerificationOTPPhone,
-  verifyUserPhone,
-  sendPasswordResetOTPPhone,
-  resetUserPassword,
-} = require("../utilities/sendOTP");
+const { signup, login, logout, verify, otp, passwordreset, reset, verifyphoneotp, phoneotp } = require("../controller/auth");
+
 
 /**SIGN UP */
 router.post("/signup", signup);
@@ -21,60 +16,20 @@ router.post("/logout", logout);
 /*REQUEST NEW VERIFICATION OTP */
 router.post("/otp", otp);
 
-/*VERIFY THE OTP */
+/**VERIFY THE OTP */
 router.post("/verify", verify);
 
-/*REQUEST NEW PHONE VERIFICATION OTP*/
-router.post("/phoneotp", async (req, res) => {
-  try {
-    const { phone_no } = req.body;
-    if (!phone_no) throw Error("A Phone number is required");
+/**REQUEST NEW PHONE VERIFICATION OTP*/
+router.post("/phoneotp", phoneotp);
 
-    const createPhoneVerificationOTP = await sendVerificationOTPPhone(phone_no);
-    console.log(createPhoneVerificationOTP);
-    res.status(200).json(createPhoneVerificationOTP);
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
-});
+/**PHONE VERIFICATION OTP*/
+router.post("/verifyphoneotp", verifyphoneotp);
 
-/* PHONE VERIFICATION OTP*/
-router.post("/verifyphoneotp", async (req, res) => {
-  try {
-    const { phone_no, otp } = req.body;
-    if (!phone_no && otp) throw Error("Empty otp details are not allowed");
+/**PASSWORD RESET REQUEST */
+router.post("/passwordreset", passwordreset );
 
-    const verifiedPhone = await verifyUserPhone({ phone_no, otp });
-    res.status(200).json({ phone_no, verified: true, verifiedPhone });
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
-});
+/**PASSWORD RESET */
+router.post("/reset", reset);
 
-/* PASSWORD RESET REQUEST */
-router.post("/passwordreset", async (req, res) => {
-  try {
-    const { phone_no } = req.body;
-    if (!phone_no) throw Error("Phone number is required");
 
-    const createPasswordResetOTP = await sendPasswordResetOTPPhone(phone_no);
-    res.status(200).json(createPasswordResetOTP);
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
-});
-
-/* PASSWORD RESET */
-router.post("/reset", async (req, res) => {
-  try {
-    const { phone_no, otp, newPassword } = req.body;
-    if (!(phone_no && otp && newPassword))
-      throw Error("Phone number is required");
-
-    await resetUserPassword({phone_no, otp, newPassword});
-    res.status(200).json({phone_no, passwordreset: true});
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
-});
 module.exports = router;
